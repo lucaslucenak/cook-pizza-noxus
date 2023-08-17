d3c3b7235d6381dac407d5257da811a10ab7d92b
Lucas Lucena <lucas.lucenak@gmail.com>
2023-08-17 00:57:34 -0300

    Add exception handler

--- src/main/java/com/teclinecg/noxus/configuration/JacksonConfiguration.java
+++ src/main/java/com/teclinecg/noxus/configuration/JacksonConfiguration.java
@@ -2,6 +2,7 @@ package com.teclinecg.noxus.configuration;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,6 @@ public class JacksonConfiguration {
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
-                .configure(DeserializationFeature.DAT);
+                .registerModule(new JavaTimeModule());
     }
 }
