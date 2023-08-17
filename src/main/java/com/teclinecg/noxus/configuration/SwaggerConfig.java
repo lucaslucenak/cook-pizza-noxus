de4e7d66d7627b1b9606ef16ac83965103ebfc6a
Lucas Lucena <lucas.lucenak@gmail.com>
2023-08-17 15:53:38 -0300

    Update pom.xml

--- src/main/java/com/teclinecg/noxus/configuration/SwaggerConfig.java
+++ src/main/java/com/teclinecg/noxus/configuration/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.teclinecg.noxus.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
